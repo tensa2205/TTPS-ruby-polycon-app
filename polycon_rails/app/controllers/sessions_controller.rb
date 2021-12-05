c921d710e467cf69b3ebe6d71b732baa3c1a1990
tensa2205 <borlandi_diego@hotmail.com>
2021-12-04 23:41:52 -0300

    Modificados eetilos en login

--- polycon_rails/app/controllers/sessions_controller.rb
+++ polycon_rails/app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
         user = User.find_by(name: params[:session][:name].downcase)
         if user && user.authenticate(params[:session][:password])
           session[:user_id] = user.id
-          flash[:notice] = "Logged in successfully."
+          flash.now[:notice] = "Logged in successfully."
           redirect_to root_path
         else
           flash.now[:alert] = "There was something wrong with your login details."
